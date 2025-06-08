67a5b244a1190754bd9c467b2e208652f75f4b52
scmlab <110064899+Camifryou@users.noreply.github.com>
2025-06-08 01:18:01 -0500

    v1

--- frontend/src/components/SidebarLeft.jsx
+++ frontend/src/components/SidebarLeft.jsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
-  FaUserCircle,
-  FaSignInAlt,
   FaTools,
   FaUsers,
   FaCogs,
 } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
-import { logout, reset } from "../features/auth/authSlice";
 
 const SidebarLeft = () => {
-  const { user } = useSelector((state) => state.auth);
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const logoutUser = () => {
-    dispatch(logout());
-    dispatch(reset());
-    navigate("/");
-  };
-
   return (
     <div className="sidebar">
       <div className="logo">
@@ -50,16 +35,6 @@ const SidebarLeft = () => {
           </li>
         </ul>
       </nav>
-
-      <div className="user-info">
-        <div className="user-detail">
-          <FaUserCircle className="user-icon" />
-          <span className="user">{user.name}</span>
-        </div>
-        <button className="logout-btn" onClick={logoutUser}>
-          <FaSignInAlt className="logout-icon" />
-        </button>
-      </div>
     </div>
   );
 };
--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -5,10 +5,8 @@ const httpRequest = axios.create({
         "Accept": "application/json",
         "Content-Type": "application/json",
     },
-    baseURL: process.env.NODE_ENV === 'production' 
-        ? '/api'
-        : 'http://localhost:8000/api',
-    withCredentials: true,
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
+    withCredentials: false,
 })
 
 export default httpRequest
